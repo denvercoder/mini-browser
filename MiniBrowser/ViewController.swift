07c528cce67f4908528be62390a48bda500c5c94
Tim Myers <realdenvercoder@gmail.com>
2017-07-16 12:29:21 -0600

    add open website functions

--- MiniBrowser/ViewController.swift
+++ MiniBrowser/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func openTapped() {
+        
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "itunesconnect.apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "developer.apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "admob.com", style: .default, handler: openPage))
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
     }
-
+    
+    func openPage(action: UIAlertAction!) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
 
